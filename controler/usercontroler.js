50c88084e08f2264cdab8aaa1f21de7d6705cadc
john <yohannesyeneakal1@gmail.com>
2024-12-20 23:27:42 +0300

    goo

--- controler/usercontroler.js
+++ controler/usercontroler.js
@@ -1,35 +1,71 @@
 import dbconn from '../db/dbconfig.js';
-export const register = async (req, res ) => {
+import bcrypt from 'bcrypt'
+export const register = async (req, res) => {
     try {
         const { username, firstname, lastname, email, password } = req.body;
-        
+
         if (!username || !firstname || !lastname || !email || !password) {
             return res.status(400).json({ msg: 'Please fill in all required fields' });
         }
 
-        // Simulate an asynchronous operation, such as a database query
-        const [user] = await dbconn.execute(
-            'INSERT INTO users (username, first_name, last_name, email, password) VALUES (?, ?, ?, ?, ?)',
-            [username, firstname, lastname, email, password]
-        );
+        // Insert the user into the database
+        
 
-        res.status(201).json('Welcome to the registration page! Registration successful!');
+        // Fetch the user details to return in the response
+        const [user] = await dbconn.query('SELECT username, user_id FROM users WHERE username = ? OR email = ?', [username, email]);
+        
+             if(user.length>0){
+                 return res.status(400).json({msg:"already ready register"})
+             }
+            //  password validation
+            if(password.length < 8){
+                res.status(400).json({msg:"passord length must be at least eigt character"})
+            }
+            //    generate hash password
+               const salt = await bcrypt.genSalt(7);
+                  const hashpassword  = await bcrypt.hash(password,salt)
+            //   insert user in database
+             await dbconn.execute(
+                'INSERT INTO users (username, first_name, last_name, email, password) VALUES (?, ?, ?, ?, ?)',
+                [username, firstname, lastname, email, hashpassword]
+            );
+            res.status(201).json( {msg:"user is created"})
+             
     } catch (err) {
-        res.status(500).json('An error occurred during registration.');
+        res.status(500).json({ msg: 'An error occurred during registration.' });
     }
 };
-
       
 
 
 
 
 
-
+//    login functionalit
 
 export const login = async(req, res) => {
-    res.send('Welcome to the login page!');
+    const {  email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ msg: 'Please fill in all required fields' });
+    }
+    try {
+        const [user] = await dbconn.query('SELECT username, user_id, password FROM users WHERE  email = ?', [ email]);
+            if(user.length == 0){
+                return res.json({ msg: 'invalid credintaly  ' });
+            }
+        const ismatch = await bcrypt.compare(password, user[0].password)
+        if(!ismatch){
+
+            return res.json({ msg:"invalid crenintial"})
+        }
+        return res.json({ user:user})
+
+    } catch (err) {
+        res.status(500).json({ msg: 'something is wrong' });
+    }
 };
+
 
 export const check = async(req, res) => {
     res.send('Welcome to the user check page!');
