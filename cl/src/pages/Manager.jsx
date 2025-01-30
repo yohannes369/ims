cd38620e9cae80d3128b415589b857eb4a460ee2
john <yohannesyeneakal1@gmail.com>
2025-01-30 20:30:03 +0300

    manager and register updated

--- cl/src/pages/Manager.jsx
+++ cl/src/pages/Manager.jsx
@@ -6,7 +6,7 @@ const Managerpage = () => {
         <div>
             
         <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 p-4">
-            <h1 className="text-4xl font-bold text-white mb-4 animate-bounce">Managerpage Dashboard</h1>
+            <h1 className="text-4xl font-bold text-white mb-4 animate-bounce">stotck clerk Dashboard</h1>
             <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
                 <Logout /> {/* Add the Logout component */}
                 
